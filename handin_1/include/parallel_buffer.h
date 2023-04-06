3c852ab0440d36cffcf36fcc89c077fb0df593c8
aske-w <aske@wachs.dk>
2023-04-06 17:43:25 +0200

    use ref when possible, use const for memory safety

--- handin_1/include/parallel_buffer.h
+++ handin_1/include/parallel_buffer.h
@@ -1,11 +1,11 @@
 #include <mutex>
 
-typedef std::vector<DataTuple>* Chunk;
-typedef std::vector<Chunk>* Partition;
-typedef std::vector<Partition>* Buffers;
+typedef std::vector<const DataTuple *const> Chunk;
+typedef std::vector<Chunk *const> Partition;
+typedef std::vector<Partition *const> Buffers;
 
 namespace ParallelBuffer
 {
     void run(std::vector<DataTuple> &data_tuples, const int THREADS, const int hashbits, const int PARTITIONS);
-    void buf_worker(std::vector<DataTuple> & tuples, Buffers buffers, std::vector<std::mutex> *mutexes, int start, int end, int hash_bits, int chunk_size);
+    void buf_worker(std::vector<DataTuple> &tuples, Buffers &buffers, std::vector<std::mutex> &mutexes, const int start, const int end, const int hash_bits, const int chunk_size);
 }
