2cae8d016d9c0147245f05b8da5f117f867daea6
aske-w <aske@wachs.dk>
2023-04-30 11:42:33 +0200

    research
    Co-authored-by: Alexander Wermuth <alexwer2@gmail.com>

--- handin_2/encode.sh
+++ handin_2/encode.sh
@@ -9,6 +9,28 @@ ffmpeg -i input.mp4 -vcodec NVENC -crf 24 output.mp4 --enable-nvenc
 
 ffmpeg -hwaccel cuda -hwaccel_output_format cuda -extra_hw_frames 4 -i cut.mp4 -c:v hevc_nvenc -crf 24 output.mp4
 
+# Encode
+ffmpeg -hwaccel cuda -hwaccel_output_format cuda -i input.mp4 -c:v h264_nvenc -b:v 5M output.mp4
+# -hwaccel cuda -hwaccel_output_format cuda
+# Decode on CPU and transfer to GPU - max PCI throughput
+ffmpeg -hide_banner \
+-c:v h264_cuvid -i /dev/shm/cut.mp4 -vf "hwupload_cuda" -t 60 -f null - \
+-c:v h264_cuvid -i /dev/shm/cut.mp4 -vf "hwupload_cuda" -t 60 -f null - \
+-c:v h264_cuvid -i /dev/shm/cut.mp4 -vf "hwupload_cuda" -t 60 -f null - \
+-c:v h264_cuvid -i /dev/shm/cut.mp4 -vf "hwupload_cuda" -t 60 -f null - \
+-c:v h264_cuvid -i /dev/shm/cut.mp4 -vf "hwupload_cuda" -t 60 -f null - \
+-c:v h264_cuvid -i /dev/shm/cut.mp4 -vf "hwupload_cuda" -t 60 -f null - \
+-c:v h264_cuvid -i /dev/shm/cut.mp4 -vf "hwupload_cuda" -t 60 -f null - \
+-c:v h264_cuvid -i /dev/shm/cut.mp4 -vf "hwupload_cuda" -t 60 -f null - \
+-c:v h264_cuvid -i /dev/shm/cut.mp4 -vf "hwupload_cuda" -t 60 -f null - \
+-c:v h264_cuvid -i /dev/shm/cut.mp4 -vf "hwupload_cuda" -t 60 -f null - \
+-c:v h264_cuvid -i /dev/shm/cut.mp4 -vf "hwupload_cuda" -t 60 -f null - \
+-c:v h264_cuvid -i /dev/shm/cut.mp4 -vf "hwupload_cuda" -t 60 -f null - \
+-c:v h264_cuvid -i /dev/shm/cut.mp4 -vf "hwupload_cuda" -t 60 -f null - \
+-c:v h264_cuvid -i /dev/shm/cut.mp4 -vf "hwupload_cuda" -t 60 -f null - \
+-c:v h264_cuvid -i /dev/shm/cut.mp4 -vf "hwupload_cuda" -t 60 -f null -
+# https://developer.nvidia.com/blog/nvidia-ffmpeg-transcoding-guide/
+
 lspci -vv
 
 dmidecode
