a23c5984d81ca8aedefc8a95f9c02e1de6878fc7
Alexander Wermuth <alexwer2@gmail.com>
2023-05-03 09:48:52 +0200

    Update monitor.py

--- handin_2/monitor.py
+++ handin_2/monitor.py
@@ -10,17 +10,35 @@ import random
 dt_string = datetime.now().strftime("%d_%m_%Y")
 DIR_NAME = os.path.join(os.path.dirname(__file__), dt_string)
 STD_ERR = os.path.join(DIR_NAME, "errors.txt")
-FOLDS = 17
+FOLDS = 11
 NUM_OF_TRIALS = 5
 TRIAL_PREFIX= "#Trial:"
 
 
-def get_ffmpeg_cmd(out_file:str, in_n: int):
+def get_ffmpeg_cmd_pci_stess_test(out_file:str, in_n: int):
     base = ["time","-f", "%e", "--output", out_file, 
               "ffmpeg", "-loglevel", "error", "-hide_banner"]
 
     for _ in range(in_n):
         base += ["-c:v", "h264_cuvid", "-i", "/dev/shm/cut.mp4", "-vf", "'hwupload_cuda'", "-t", "60", "-f", "null", "-"] 
+
+    return base
+
+def get_ffmpeg_cmd_cpu(out_file:str, in_n: int):
+    base = ["time","-f", "%e", "--output", out_file, 
+              "ffmpeg", "-loglevel", "error", "-hide_banner"]
+
+    for _ in range(in_n):
+        base += ["-i", "/dev/shm/cut.mp4", "-t", "60", "-f", "null", "-"] 
+
+    return base
+
+def get_ffmpeg_cmd_gpu(out_file:str, in_n: int):
+    base = ["time","-f", "%e", "--output", out_file, 
+              "ffmpeg", "-loglevel", "error", "-hide_banner"]
+
+    for _ in range(in_n):
+        base += ["-c:v", "h264_cuvid", "-i", "/dev/shm/cut.mp4", "-t", "60", "-f", "null", "-"] 
 
     return base
 
@@ -41,15 +59,17 @@ def parse_gpu_state(GPU_OUT_FILE:str):
     sm:list[list[int]] = [[]] * NUM_OF_TRIALS
     encoders:list[list[int]] = [[]] * NUM_OF_TRIALS
     decoders:list[list[int]] = [[]] * NUM_OF_TRIALS
-    trial_idx = 0
+    trial_idx = -1
     for idx, line in enumerate(lines):
         stats = line.split()
-        if stats[0] == "#":
+        if len(stats) == 0:
             continue
         if line.startswith(TRIAL_PREFIX):
             # First line in file
             if len(rxs) != 0:
                 trial_idx += 1
+            continue
+        if stats[0] == "#":
             continue
         if len(stats) > 4:
             sm[trial_idx].append(int(stats[4]))
@@ -94,10 +114,7 @@ def generate_pci_graph():
             prefix = f"_{pci_gen}_16_{fold}"
             GPU_OUT_FILE = get_gpu_stats_path(prefix)
             rxs, txs, _, _, _ = parse_gpu_state(GPU_OUT_FILE)
-
-            rxs_avg = np.average(rxs, axis=1)
-            txs_avg = np.average(txs, axis=1)
-            _sum = list(map(lambda _x: _x[0] + _x[1], zip(txs_avg, rxs_avg)))
+            _sum = list(map(lambda _x: _x[0] + _x[1], zip(rxs[0], txs[0])))
             
             x = [i for i in range(len(_sum))] 
             x_ticks = max(x_ticks, len(x))
@@ -109,42 +126,57 @@ def generate_pci_graph():
         noun = "streams" if fold > 1 else "stream"
         plt.title(f"PCIe throughput for {fold} {noun}")
         plt.grid(visible=True, which='both', axis='both', linestyle = '--', linewidth = 0.5)
-        plt.savefig(os.path.join(DIR_NAME, f"pci_{prefix}.png"))
+        plt.savefig(os.path.join(DIR_NAME, f"pci_seconds_graph_{prefix}.png"))
         plt.clf()
 
+
+
 def main():
+    args = sys.argv[1:]
+
+    if len(args) == 0:
+        raise Exception("Please provide type")
+    type_run = args[0]
+
     if not os.path.exists(DIR_NAME):
         os.mkdir(DIR_NAME, )
     pci_gen = sp.check_output(["nvidia-smi", "--query-gpu", "pcie.link.gen.max", "--format", "csv,noheader,nounits"]).decode(sys.stdout.encoding).strip()
     pci_width = sp.check_output(["nvidia-smi", "--query-gpu", "pcie.link.width.max", "--format", "csv,noheader,nounits"]).decode(sys.stdout.encoding).strip()
 
-    for i in range(8, FOLDS):
-        afterfix = f"_{pci_gen}_{pci_width}_{i}"
+    for i in range(1, FOLDS):
+        afterfix = f"_{type_run}_{pci_gen}_{pci_width}_{i}"
         REPORT_OUT_FILE = os.path.join(DIR_NAME, f"report{afterfix}.txt")
         GPU_OUT_FILE = get_gpu_stats_path(afterfix)
-        FFMPEG_CMD = get_ffmpeg_cmd(REPORT_OUT_FILE, i)
+        FFMPEG_CMD = get_ffmpeg_cmd_pci_stess_test(REPORT_OUT_FILE, i) if type_run == "stress_test" else get_ffmpeg_cmd_gpu(REPORT_OUT_FILE, i) if type_run == "gpu" else get_ffmpeg_cmd_cpu(REPORT_OUT_FILE, i)
+        ffmpeg_out = open(REPORT_OUT_FILE, "a+")
+        errors_out = open(STD_ERR, "a+")
+        gpu_out = open(GPU_OUT_FILE, "a+")
 
         for trial in range(NUM_OF_TRIALS):
             trial = f"{TRIAL_PREFIX} {trial} for {afterfix}"
             print(trial)
-            ffmpeg_out = open(REPORT_OUT_FILE, "a+")
-            errors_out = open(STD_ERR, "w")
-            gpu_out = open(GPU_OUT_FILE, "a+")
-            gpu_out.write(f"{trial}\n")
-    
+            print("", file=gpu_out)
+            print(f"{trial}", file=gpu_out)
+            gpu_out.flush()
             ffmpeg = sp.Popen(FFMPEG_CMD, stdout=ffmpeg_out, stderr=errors_out)
             nvi_process = sp.Popen(SMI_CMD, stdout=gpu_out, stderr=errors_out)
             ffmpeg.wait()
             nvi_process.kill()
     
-            gpu_out.close()
-            ffmpeg_out.close()
-            errors_out.close()
+            gpu_out.flush()
+            ffmpeg_out.flush()
+            errors_out.flush()
+
+        gpu_out.close()
+        ffmpeg_out.close()
+        errors_out.close()
 
         generate_report(f"PCI: {afterfix}", REPORT_OUT_FILE, GPU_OUT_FILE)
     
     # generate_pci_graph()
 
+
 if __name__ == '__main__':
     print("hello")
     main()
+    
