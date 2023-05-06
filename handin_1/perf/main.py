777b112ee6ecd964f70e78a02fba01323a610619
Alexander Wermuth <alexwer2@gmail.com>
2023-05-06 16:03:11 +0200

    Working stuff
    Co-Authored-By: aske-w <72822902+aske-w@users.noreply.github.com>

---
+++ handin_1/perf/main.py
@@ -0,0 +1,91 @@
+import re
+import glob, os
+import numpy as np
+import matplotlib.pyplot as plt
+
+REGEX = "(?P<name>[a-zA-Z][a-zA-Z0-9)( -]*)\#\s*(?P<value>\d+\.\d+)"
+
+
+ALGOS = ["concurrent", "parallel"]
+FILES:list[str] = []
+stats:dict[str, list[float]] = {}
+for algo in ALGOS:
+    for root, dirs, files in os.walk(f"./{algo}"):
+        for file_path in files:
+            print(file_path)
+            print(root)
+            if file_path.endswith(".txt"):
+                FILES.append(os.path.join(root, file_path))
+
+# print(FILES)
+
+for file_path in FILES:
+    dir_split = file_path.split("/")
+    meta = dir_split[3].split("-")
+    algo = dir_split[1]
+    thread = meta[0]
+    hashbit = meta[1]
+    print(f"algo: {algo}")
+    with open(file_path) as f:
+        lines = f.readlines()
+        for l in lines:
+            res = re.search(REGEX, l)
+            if res is None:
+                continue
+
+            name = res.group('name').strip()
+            value = res.group('value')
+            # print(f"name: {name}, value: {value}")
+            key = f"{algo}/{thread}/{hashbit}/{name}"
+            if stats.get(key) is None:
+                stats[key] = []
+            stats[key].append(float(value))
+    
+
+# print(stats)
+data_map:dict[str, list[float]] = {}
+
+for key, val in stats.items():
+    meta = key.split("/")
+    algo = meta[0]
+    thread = meta[1]
+    name = meta[3]
+    # print(thread)
+    # print(name)
+    avg = np.mean(val)
+    new_key = f"{algo}/{thread}/{name}"
+    if data_map.get(new_key) is None:
+        data_map[new_key] = []    
+    data_map[new_key].append(avg)
+
+# print(data_map)
+
+HASH_BITS = list(range(1,2))
+x_ticks = HASH_BITS
+map_idx = {}
+plot_idx = 0
+for a_t, h_avg in data_map.items():
+    meta = a_t.split("/")
+    algo = meta[0]
+    thread = meta[1]
+    name = meta[2]
+    # print(f"algo: {algo}")
+    key = f"{algo}/{name}"
+    if map_idx.get(key) is None:
+        map_idx[key] = plot_idx
+        plot_idx +=1
+    plt.figure(map_idx[key])
+    plt.plot(x_ticks[:17], h_avg[:17], label=f"Thread {thread}")
+    plt.xticks(x_ticks)
+    plt.legend()
+
+# Plot graphs
+for key, idx in map_idx.items():
+    # print(f"name: {name} idx: {idx}")
+    meta = key.split("/")
+    algo = meta[0]
+    name = meta[1]
+    plt.figure(idx)
+    plt.title(f"{algo}-{name}")
+    plt.savefig(f"pictures/{algo}-{name}.png")
+    plt.close()
