dfc078ceafbfbc03adcbd4d175d614a152d67610
Alexander Wermuth <alexwer2@gmail.com>
2023-05-13 12:27:40 +0200

    Added new graphs
    Co-Authored-By: aske-w <72822902+aske-w@users.noreply.github.com>

--- handin_2/build_graphs.py
+++ handin_2/build_graphs.py
@@ -15,22 +15,24 @@ def read_report(path:str):
     tx = None
     rx = None
 
+    temp_idx = 0
     for idx, line in enumerate(lines):
-        if idx < NUM_OF_TRIALS:
+        words = line.strip().split(" ")
+        if words[0] != "PCI:":
             times.append(float(line.strip()))
             continue
-        words = line.strip().split(" ")
         temp = float(words[len(words) - 1])
-        if idx == (NUM_OF_TRIALS - 1) + 1:
+        if temp_idx == 0:
             tx = temp
-        elif idx == (NUM_OF_TRIALS - 1) + 2:
+        elif temp_idx == 1:
             rx = temp
-        elif idx == (NUM_OF_TRIALS - 1) + 3:
+        elif temp_idx == 2:
             sm = temp
-        elif idx == (NUM_OF_TRIALS - 1) + 4:
+        elif temp_idx == 3:
             encoder = temp
-        elif idx == (NUM_OF_TRIALS - 1) + 5:
+        elif temp_idx == 4:
             decoder = temp
+        temp_idx += 1
 
         
         
@@ -42,7 +44,8 @@ def read_report(path:str):
     return time_avg, sm, decoder, encoder, tx, rx
 
 
-def build_stress_test_bar_chart(test_type:str):
+def build_stress_test_bar_chart(test_type:str, report_name = None, dir = DATA_FOLDER, _from = 1, to = 4, _label = None):
+    report_name = test_type if report_name == None else report_name
     pci_width = 16
     times:list[list[int]] = [ []*3 for i in range(3)]
     sms:list[list[int]] = [ []*3 for i in range(3)]
@@ -50,10 +53,10 @@ def build_stress_test_bar_chart(test_type:str):
     encoders:list[list[int]] = [ []*3 for i in range(3)]
     txs:list[list[int]] = [ []*3 for i in range(3)]
     rxs:list[list[int]] = [ []*3 for i in range(3)]
-    for pci_gen in range(1, 4):
+    for pci_gen in range(_from, to):
         gen_idx = pci_gen - 1
         for fold in range(1, FOLDS):
-            p = os.path.join(DATA_FOLDER, f"report_{test_type}_{pci_gen}_{pci_width}_{fold}.txt")
+            p = os.path.join(dir, f"report_{report_name}_{pci_gen}_{pci_width}_{fold}.txt")
             time_avg, sm, decoder, encoder, tx, rx = read_report(p)
             times[gen_idx].append(time_avg)
             sms[gen_idx].append(sm)
@@ -64,14 +67,14 @@ def build_stress_test_bar_chart(test_type:str):
 
     bar_width = 0.25
     x = None
-    for pci_gen in range(0, 3):
+    for pci_gen in range(_from - 1, to - 1):
         if x is not None:
             x = [_x + bar_width for _x in x]
         else:
             x = [i + bar_width for i in range(0, FOLDS - 1)]
 
         color = 'r' if pci_gen == 0 else 'b' if pci_gen == 1 else 'g'
-        label = f"PCI-E x gen: {pci_gen + 1}"
+        label = f"PCI-E x gen: {pci_gen + 1}" if _label == None else _label
 
         plt.figure(0)
         plt.title(f"Average time to decode in seconds")
@@ -97,7 +100,7 @@ def build_stress_test_bar_chart(test_type:str):
         plt.title(f"Average throughput recieved over PCI-E in GiB/s")
         plt.bar(x, rxs[pci_gen], color=color, width=bar_width, label=label)
 
-        if test_type == "stress_test" or test_type == "gpu":
+        if test_type == "stress_test" or test_type == "gpu" or test_type == "two_process":
             plt.figure(6)
             plt.title(f"Average throughput recieved and transmitted over PCI-E in GiB/s")
             plt.bar(x, [a + b for a, b in zip(rxs[pci_gen], txs[pci_gen])], color=color, width=bar_width, label=label)
@@ -105,7 +108,7 @@ def build_stress_test_bar_chart(test_type:str):
     names = ["time", "sm", "dec", "enc", "tx", "rx"]
 
 
-    if test_type == "stress_test" or test_type == "gpu":
+    if test_type == "stress_test" or test_type == "gpu" or "two_process":
         names.append("total_pci")
 
     for i, n in enumerate(names):
@@ -127,20 +130,26 @@ def build_stress_test_bar_chart(test_type:str):
 def build_cpu_time_graph():
     pci_gen = 3
     pci_width = 16
-    times = []
+    cpu_times = []
+    gpu_times = []
     for fold in range(1, FOLDS):
-        p = os.path.join(DATA_FOLDER, f"report_cpu_{pci_gen}_{pci_width}_{fold}.txt")
-        time_avg, _, _, _, _, _ = read_report(p)
-        times.append(time_avg)
+        cpu_p = os.path.join(DATA_FOLDER, f"report_cpu_{pci_gen}_{pci_width}_{fold}.txt")
+        gpu_p = os.path.join(DATA_FOLDER, f"report_gpu_{pci_gen}_{pci_width}_{fold}.txt")
+        time_avg_cpu, _, _, _, _, _ = read_report(cpu_p)
+        time_avg_gpu, _, _, _, _, _ = read_report(gpu_p)
+        cpu_times.append(time_avg_cpu)
+        gpu_times.append(time_avg_gpu)
 
     plt.clf()
     plt.cla()
     bar_width = 0.25
     x = [i + bar_width for i in range(0, FOLDS - 1)]
-    plt.bar(x, times, label="cpu", width = bar_width)
-    plt.legend()
+    plt.bar(x, cpu_times, label="CPU", width = bar_width)
+    x = [_x + bar_width for _x in x]
+    plt.bar(x, gpu_times, label="GPU", width = bar_width)
+    plt.legend(loc="lower right")
     plt.grid()
-    plt.title("Average time to decode on CPU in seconds")
+    plt.title("Average time to decode in seconds on CPU and GPU using PCI gen 3")
     plt.xlabel("Number of streams")
     plt.ylabel("Seconds")
     plt.xticks(x, [i for i in range(1, FOLDS)])
@@ -156,4 +165,5 @@ args = ["stress_test", "gpu"]
 
 build_stress_test_bar_chart("stress_test")
 build_stress_test_bar_chart("gpu")
+build_stress_test_bar_chart("two_process", report_name="stress_test", dir = os.path.join("./new_mps_data", "2-procs-mps-12_05_2023"), _from=2, to=3, _label = "PCI gen 3")
 build_cpu_time_graph()
